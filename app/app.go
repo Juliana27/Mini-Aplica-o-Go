ef54823f95788a1d6223a9be3a26015493159b0a
Juliana27 <juliana.brito@mercadolivre.com>
2022-04-19 12:28:52 -0300

    iniciando aplicação

---
+++ app/app.go
@@ -0,0 +1,13 @@
+package app
+
+import "github.com/urfave/cli"
+
+//Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de comando"
+	app.Usage = "Busca IPs e Nomes de servidor na internet"
+
+
+	return app
+}
