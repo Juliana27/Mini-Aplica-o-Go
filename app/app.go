bb7fc819e6250d6aa3699a7e3c1819c4a6571ec3
Juliana27 <juliana.brito@mercadolivre.com>
2022-04-19 16:08:50 -0300

    funcao buscar servidores

--- app/app.go
+++ app/app.go
@@ -13,20 +13,41 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de comando"
 	app.Usage = "Busca IPs e Nomes de servidor na internet"
 
+	flags := []cli.Flag {
+		cli.StringFlag{
+			Name: "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command {
 		{
 			Name: "ip",
 			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag {
-				cli.StringFlag{
-					Name: "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Flags: flags,
 			Action: buscarIps,
+		},
+		{
+			Name: "servidores",
+			Usage: "Busca o nome dos servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
 		},
 	}
 	return app
+}
+
+func buscarServidores(c *cli.Context)  {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
 
 func buscarIps(c *cli.Context) {
